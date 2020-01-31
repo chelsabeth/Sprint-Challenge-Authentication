1c4c9b53984e25836e80012237b517e768c7ba01
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-31 13:56:00 -0500

    tests for jokes router

--- jokes/jokes-router.test.js
+++ jokes/jokes-router.test.js
@@ -11,16 +11,18 @@ describe('jokes-router', function() {
 
     describe('GET / all jokes', function() {
         it('should return 200 OK', function() {
-            return request(server).get('/')
+            return request(server).get('/api/jokes')
+            .set('Authorization', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImNoZWxzZWEiLCJwYXNzd29yZCI6IiQyYSQwOCQ5UXJqcEo0aVFONGEuQlhwNHlPNk91UjNnT2pEQVBWOGFzUGRYQVRJa2JsQUY0TU5TdmV6ZSJ9LCJpYXQiOjE1ODA0ODk5MzksImV4cCI6MTU4MDU3NjMzOX0.7Ir2UW1-rM8tRjzARZge1ZLBt80hsDUo2MT_yPFs_E0")
             .then(res => {
-                expect(res.status).toBe(404); // need to fix these, not sure why it returns 404
+                expect(res.status).toBe(200); // need to fix these, not sure why it returns 404
             })
         })
     })
 
     describe('GET / all jokes err', function() {
         it('should not return 500', function() {
-            return request(server).get('/')
+            return request(server).get('/api/jokes')
+            .set('Authorization', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImNoZWxzZWEiLCJwYXNzd29yZCI6IiQyYSQwOCQ5UXJqcEo0aVFONGEuQlhwNHlPNk91UjNnT2pEQVBWOGFzUGRYQVRJa2JsQUY0TU5TdmV6ZSJ9LCJpYXQiOjE1ODA0ODk5MzksImV4cCI6MTU4MDU3NjMzOX0.7Ir2UW1-rM8tRjzARZge1ZLBt80hsDUo2MT_yPFs_E0")
             .then(res => {
                 expect(res.status).not.toBe(500);
             })
